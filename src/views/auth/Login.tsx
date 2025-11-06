ec409b9a5c8c05492d9f888dfa40f60c84f3f932
Dilovar <dilovarmizrobov@gmail.com>
2025-11-06 18:17:40 +0500

    Removed "Abad technology" text from login page

--- src/views/auth/Login.tsx
+++ src/views/auth/Login.tsx
@@ -124,39 +124,13 @@ function LoginView() {
                     <CardMedia
                         className={classes.media}
                         image="/static/auth.png"
-                        title="DuobTMS | Abad Technologies"
+                        title="DuobTMS"
                     >
-
-                        <Box
-                            alignItems="center"
-                            display="flex"
-                            mt={3}
-                        >
-                            <Avatar
-                                className={classes.logo}
-                                alt="AbadTech"
-                                src="/static/abadtech.png"
-                            />
-                            <Box >
-                                <Typography
-                                    color="inherit"
-                                    variant="h2"
-                                >
-                                    Abad
-                                </Typography>
-                                <Typography
-                                    color="inherit"
-                                    variant="h3"
-                                >
-                                    Technologies
-                                </Typography>
-                            </Box>
-                        </Box>
                         <Typography
                             color="inherit"
-                            variant="subtitle1"
+                            variant="h2"
                         >
-                            Enterprise Software Development, Design & Consulting
+                            DuobTMS
                         </Typography>
                     </CardMedia>
                 </Card>
